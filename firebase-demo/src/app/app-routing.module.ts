37b2d91673e8392e00d501ae1e0e9be08674569d
Philipp Dorner <pdorner@gmx.net>
2019-11-12 09:50:40 +0100

    Navabar Header routing

--- firebase-demo/src/app/app-routing.module.ts
+++ firebase-demo/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursepageComponent } from './coursepage/coursepage.component';
+import { TeacherpageComponent } from './teacherpage/teacherpage.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
+const routes: Routes = [{
 
-const routes: Routes = [];
+        path:"",component: LandingpageComponent
+
+},
+
+{
+
+        path: "teacherpage",component: TeacherpageComponent
+
+}, 
+
+{
+		 path: "coursepage",component: CoursepageComponent
+
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
