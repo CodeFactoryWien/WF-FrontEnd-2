b95d00de9743ff3b16d8b5961d6a247cf954cc54
Philipp Dorner <pdorner@gmx.net>
2019-11-12 14:36:10 +0100

    names

--- firebase-demo/src/app/course/course.component.ts
+++ firebase-demo/src/app/course/course.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from "../shared/course.service";
 
+
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
--- firebase-demo/src/app/teacher-name/teacher-name.component.ts
+++ firebase-demo/src/app/teacher-name/teacher-name.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { TeacherService } from "../shared/teacher.service";
 
 @Component({
-  selector: 'app-teacher-name',
+  selector: 'teacher-name',
   templateUrl: './teacher-name.component.html',
   styleUrls: ['./teacher-name.component.scss']
 })
 export class TeacherNameComponent implements OnInit {
 
-  constructor() { }
+teacherArray=[];
+ showDeletedMessage : boolean;
+ searchText:string = "";
 
-  ngOnInit() {
-  }
+
+
+
+ constructor(public teacherService: TeacherService) { }
+
+ ngOnInit() { 
+         this.teacherService.getteacher().subscribe(
+                 (list) => {
+                         this.teacherArray = list.map( (item) => {
+                                return {
+                                        $key : item.key,
+                                        ...item.payload.val()
+                                }
+                        })
+                 });
+    
+}
+onDelete($key){
+     if(confirm("Are you sure you want to delete this teacher?")){
+        this.teacherService.deleteteacher($key);
+       this.showDeletedMessage = true;
+       setTimeout(()=> this.showDeletedMessage=false , 3000)
+     }
+   }
+   filterCondition(teacher){
+     return teacher.fullName.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1 ;
+   }
 
 }
