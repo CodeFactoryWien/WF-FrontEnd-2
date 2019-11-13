a8f0523b39e6f4b53ecf44586dc02934ff3576b1
Philipp Dorner <pdorner@gmx.net>
2019-11-13 09:36:04 +0100

    cards seperate

--- firebase-demo/src/app/coursecard/coursecard.component.ts
+++ firebase-demo/src/app/coursecard/coursecard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-coursecard',
+  selector: 'coursecard',
   templateUrl: './coursecard.component.html',
   styleUrls: ['./coursecard.component.scss']
 })
