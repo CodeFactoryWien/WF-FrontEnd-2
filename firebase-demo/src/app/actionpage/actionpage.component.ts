e1d231a5344ca1567eb35e971603255ffa84176a
Philipp Dorner <pdorner@gmx.net>
2019-11-13 10:34:12 +0100

    table for pates action jump + strategy

--- firebase-demo/src/app/actionpage/actionpage.component.ts
+++ firebase-demo/src/app/actionpage/actionpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from "../shared/course.service";
 
 @Component({
   selector: 'actionpage',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actionpage.component.scss']
 })
 export class ActionpageComponent implements OnInit {
+courseArray =[];
+ showDeletedMessage : boolean;
+ searchText:string = "";
 
-  constructor() { }
+ constructor(public courseService: CourseService) { }
 
-  ngOnInit() {
-  }
+ ngOnInit() { 
+         this.courseService.getCourse().subscribe(
+                 (list) => {
+                         this.courseArray = list.map( (item) => {
+                                return {
+                                        $key : item.key,
+                                        ...item.payload.val()
+                                }
+                        })
+                 });
+    
+}
+onDelete($key){
+     if(confirm("Are you sure you want to delete this course?")){
+        this.courseService.deleteCourse($key);
+       this.showDeletedMessage = true;
+       setTimeout(()=> this.showDeletedMessage=false , 3000)
+     }
+   }
+   filterCondition(course){
+     return course.title.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1 ;
+   }
 
 }
